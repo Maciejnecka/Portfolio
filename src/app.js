7c0eeb08f1c47745da9fd4ba803bbd9341551d49
Maciej Necka <macieknecka@gmail.com>
2024-01-03 18:07:44 +0100

    Fonts, and reset added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Reset } from 'styled-reset';
 import App from './components/App';
+import GlobalFonts from './fonts/GlobalFonts.js';
 
 const root = createRoot(document.querySelector('#root'));
-root.render(<App />);
+root.render(
+  <>
+    <Reset />
+    <GlobalFonts />
+    <App />
+  </>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,37 @@
 const path = require('path');
-// importuję bibliotękę [path] z [node.js]
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// importuję odpowiedni plugin
+
 module.exports = {
-    entry: './src/app.js',
-    // definiuje plik wejściowy
-    mode: 'development',
-    // tryb pracy
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        // definiuje ścieżką wyjściową
-        filename: 'app.min.js',
-        // definiuję nazwę pliku wyjściowego
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                // określam jakie pliki 
-                // będą brane pod uwagę
-                exclude: /node_modules/,
-                // określam wykluczenia
-                use: 'babel-loader',
-                // określam jaki [loader]
-                // ma być wykorzystany
-            }
-        ]
-        // obecnie brak dodatkowych ustawień
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            // wskazuje plik źródłowy
-            filename: 'index.html'
-            // określan nazwę dla pliku
-        })
-    ]
-}
-// eksportuję ustawienia dla webpack-a
+  entry: './src/app.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
